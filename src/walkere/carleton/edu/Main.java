891b5891da4d82ebd5a5bddd42599b6c69a27eac
Eric Walker <erictwalker18@gmail.com>
2014-11-12 08:54:21 -0600

    Created IntelliJ Project

---
+++ src/walkere/carleton/edu/Main.java
@@ -0,0 +1,17 @@
+package walkere.carleton.edu;
+
+/**
+ * BlobStudios
+ *
+ * Eric Walker
+ *
+ * CS 257 Final Project
+ * Created on 11/12/2014
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
