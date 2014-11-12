0904d6185e2882e25491dadd49722c1638c523fe
Eric Walker <erictwalker18@gmail.com>
2014-11-12 09:07:13 -0600

    Added model class

---
+++ src/walkere/carleton/edu/Model.java
@@ -0,0 +1,15 @@
+package walkere.carleton.edu;
+
+/**
+ * BlobStudios
+ *
+ * Eric Walker
+ *
+ * CS 257 Final Project
+ * Created on 11/12/2014.
+ */
+
+public class Model {
+    public Model() {
+    }
+}
